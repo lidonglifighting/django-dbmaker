40a1390665e2c462be6d7da7e71845d4ab6dfcb2
lidonglifighting <42424903+lidonglifighting@users.noreply.github.com>
2023-12-13 17:29:25 +0800

    test 2fa

--- django_dbmaker/base.py
+++ django_dbmaker/base.py
@@ -42,7 +42,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
-DBMaker Database backend for Django.
+DBMaker database backend for Django.
 """
 import datetime
 import logging
